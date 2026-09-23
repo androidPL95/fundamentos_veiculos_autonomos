14d86401677000b05bb9844a8ed61a68ae74b095
DaviZTS <davitassara@gmail.com>
2026-09-22 22:47:08 -0300

    PI implementado com erro

--- simulador/main.py
+++ simulador/main.py
@@ -6,34 +6,40 @@
 # DELT – Escola de Engenharia
 # Universidade Federal de Minas Gerais
 ########################################
-from fva_car import Car
+import class_car as cp
 import numpy as np
 import os
 os.environ["QT_QPA_PLATFORM"] = "xcb"
 import matplotlib.pyplot as plt
-plt.rcParams['figure.figsize'] = (6,8)
+plt.rcParams['figure.figsize'] = (10,10)
 
 # Globais
 parameters = {	
-				'ts'		: 10.0, 			# tempo da simulacao
+				'ts'		: 20.0, 			# tempo da simulacao
 				'save'		: True,
 				'logfile'	: 'logs/',
-				'beep'		: True,
 			}
+
+SET_VEL = 1.0
+
+# Rx = -0.0931
 	
 ########################################
 # thread de controle de velocidade
 ########################################
-def control_func(car):
+def control_func(car, integral):
 		
-	# seta direcao
-	car.set_steer(np.deg2rad(5.0*np.sin(car.t)))
+	v, w = car.get_vel()
+	erro = SET_VEL - v
+	integral = integral + erro*car.dt
+	prop = 10 * erro
+	i = 0.1 * integral
+	tot = prop + i
+	# Controlador PI: o total antes da saturacao mostra as duas contribuicoes
+	car.set_u(tot)
 
-	# atua
-	if car.t < 5.0:
-		car.set_u(0.3)
-	else:
-		car.set_u(0.0)
+	return integral, prop, i, tot
+
 		
 ########################################
 # thread de visão
@@ -54,11 +60,15 @@ def vision_func(car):
 ########################################
 if __name__ == "__main__":
 	
-	plt.figure(1)
+	plt.figure(1, figsize=(10, 15))
 	plt.ion()
 	
 	# cria comunicação com o carrinho
-	car = Car(parameters)
+	car = cp.Car(parameters)
+
+	integral = 0
+
+	control_params = []
 	
 	try:
 		# começa a simulação
@@ -71,26 +81,43 @@ if __name__ == "__main__":
 			car.step()
 			
 			# funcao de controle
-			control_func(car)
+			integral, prop, i, tot = control_func(car, integral)
+
+			data = {"t": car.t, "p": prop, "i": i, "tot": tot}
+			control_params.append(data)
 			
 			# funcao de visao
 			image = vision_func(car)
 			
 			########################################
 			# plota	
-			plt.subplot(211)
+			plt.subplot(311)
 			plt.cla()
 			plt.gca().imshow(image, cmap='gray')
-			plt.axis('off')
-			plt.title(f'Telemetria em t={car.t:.1f}s')
+			plt.title(f't = {car.t:.2f}, v = {car.get_vel()[0]:.2f}, u = {car.u:.2f}')
 			
-			plt.subplot(212)
+			plt.subplot(312)
 			plt.cla()
 			t = [traj['t'] for traj in car.traj]
 			v = [traj['v'] for traj in car.traj]
 			plt.plot(t,v)
 			plt.ylabel('v[m/s]')
 			plt.xlabel('t[s]')
+
+			plt.subplot(313)
+			plt.cla()
+			t_control = [data["t"] for data in control_params]
+			p = [data["p"] for data in control_params]
+			i = [data["i"] for data in control_params]
+			tot = [data["tot"] for data in control_params]
+			plt.plot(t_control, p, label='P', color='red')
+			plt.plot(t_control, i, label='I')
+			plt.plot(t_control, tot, label='PI')
+			plt.ylabel('u')
+			plt.xlabel('t[s]')
+			plt.legend()
+			
+			plt.tight_layout()
 			
 			plt.show()
 			plt.pause(0.01)
@@ -98,9 +125,6 @@ if __name__ == "__main__":
 		# salva
 		if parameters['save']:
 			car.save()
-	
-	except KeyboardInterrupt:
-		print("\nMissao interrompida pelo usuario.")
-		
+			
 	finally:
 		car.close()
